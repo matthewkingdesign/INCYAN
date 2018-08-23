50a4e408a83591ce9fe7b79e338d1efb0606ecf0
matthewkingdesign <hello@matthewking.design>
2018-08-23 21:11:16 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// var path = document.querySelector('.st2');
+// var length = path.getTotalLength();
+// console.log(length);
+$(document).ready(function(){
+  $(".main-nav-break").hide().show("slide", { direction: "up" }, 300);
+  $("#main-nav-indago").hide().show("slide", { direction: "right" }, 1000);
+  $("#main-nav-contact").hide().show("slide", { direction: "left" }, 1000);
+  $("#main-nav-about").hide().show("slide", { direction: "right" }, 1000);
+  $("#main-nav-research").hide().show("slide", { direction: "right" }, 1000);
+  $("#main-nav-clients").hide().show("slide", { direction: "left" }, 1000);
+});
+
+
+
+// $("#home-button").fadeIn(1000);
+// $("#main-nav-about").fadeIn(1600);
+// $("#main-nav-research").fadeIn(1000);
+// $("#main-nav-clients").fadeIn(1000);
+// $("#main-nav-contact").fadeIn(1600);
+// $(".st0").hide().fadeIn(1600);
+// simple height toggle animation
+// $("#homepage-hamburger").click(function() {
+//   $("#homepage-content").animate({
+//     height: 'toggle'
+//   });
+// });
+// $("#homepage-hamburger").mouseenter(function(){
+//   $(this).addClass("shake").mouseleave(function(){
+//     $(this).removeClass("shake");
+//   });
+// });
+//
+// $("#homepage-hamburger").click(function(){
+//     var div = $("#homepage-content");
+//     div.animate({height: 'toggle'}, "slow");
+//     div.animate({top: '100px'}, "slow");
+//     $("#homepage-blurb").animate({width:'33vw'},"slow");
+// });
+
+// CHANGE CURSOR TO HAMBURGER on Click event
+// $("#homepage-hamburger").click(function(){
+// if($(".container").hasClass("zoomed")){
+//   $(".container").removeClass("zoomed");
+// }else{
+//     $(".container").addClass("zoomed");
+// }
+// });
