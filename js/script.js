f37c959811fc2243c7a4800c5b66ef8c9c113807
matthewkingdesign <hello@matthewking.design>
2018-08-24 01:18:52 +0100

    sorted research tool page layout and initial content.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var hidden = $(".research-title, .arrows, #research-description, #research-cta");
+hidden.hide();
+
 $(document).ready(function(){
   // Targeting links to slide in individually after hiding them individually.
   $(".main-nav-break").hide().show("slide", { direction: "up" }, 1000);
@@ -6,4 +9,9 @@ $(document).ready(function(){
   $("#main-nav-about").hide().show("slide", { direction: "right" }, 1000);
   $("#main-nav-research").hide().show("slide", { direction: "right" }, 1000);
   $("#main-nav-clients").hide().show("slide", { direction: "left" }, 1000);
+
+
+  $(".research-title").show("slide", { direction: "left" }, 1000);
+  $(".arrows, #research-description").fadeIn(1500);
+  $("#research-cta").fadeIn(2500);
 });
